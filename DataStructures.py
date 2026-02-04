81df42fed327dedf811f784767505212398e2de1
Yamunika Chintapanti <199625828+yamunikac@users.noreply.github.com>
2026-02-04 19:33:22 +0530

    Add examples for list, tuple, set, and dictionary
    Added examples demonstrating the usage of list, tuple, set, and dictionary in Python, including their outputs and types.

--- DataStructures.py
+++ DataStructures.py
@@ -1,1 +1,45 @@
+#List
+fruits = ["apple", "banana", "mango", "apple"]
+print(fruits)
+print(fruits[1])
+print(type(fruits))
+#Output
+#['apple', 'banana', 'mango', 'apple']
+#banana
+#<class 'list'>
+
+
+#Tuple
+numbers = (10, 20, 30, 40)
+print(numbers)
+print(numbers[2])
+print(type(numbers))
+#Output
+#(10, 20, 30, 40)
+#30
+#<class 'tuple'>
+
+
+#Set
+values = {1, 2, 3, 3, 4}
+print(values)
+print(type(values))
+#Output
+#{1, 2, 3, 4}
+#<class 'set'>
+
+
+#Dictionary
+student = {
+    "name": "Yamunika",
+    "age": 20,
+    "course": "CSE"
+}
+print(student)
+print(student["name"])
+print(type(student))
+#Output
+#{'name': 'Yamunika', 'age': 20, 'course': 'CSE'}
+#Yamunika
+#<class 'dict'>
 
