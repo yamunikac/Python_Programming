d1248a9b9933c23c25a88e2b8bed44c4cfb3dc80
Yamunika Chintapanti <199625828+yamunikac@users.noreply.github.com>
2026-02-04 19:43:05 +0530

    Update Operations.py

--- Operations.py
+++ Operations.py
@@ -1,1 +1,82 @@
+#Arithmetic Operators
+a = 10
+b = 3
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a % b)   # Modulus
+print(a ** b)  # Exponent
+print(a // b)  # Floor Division
+#Output
+#13
+#7
+#30
+#3.3333333333333335
+#1
+#1000
+#3
+
+
+#Comparison Operators
+x = 5
+y = 10
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+#Output
+#False
+#True
+#False
+#True
+#False
+#True
+
+
+#Logical Operators
+p = True
+q = False
+print(p and q)
+print(p or q)
+print(not p)
+#Output
+#False
+#True
+#False
+
+
+#Assignment Operators
+num = 10
+num += 5
+print(num)
+num *= 2
+print(num)
+#Output
+#15
+#30
+
+
+#Membership Operators
+text = "Python"
+print("P" in text)
+print("z" not in text)
+#Output
+#True
+#True
+
+
+#Identity Operators
+a = [1,2,3]
+b = a
+c = [1,2,3]
+print(a is b)
+print(a is c)
+print(a == c)
+#Output
+#True
+#False
+#True
 
